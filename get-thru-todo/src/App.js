8247641529bb434d1dc7b4290e91247a35cc0a0b
Rohan-154 <86555869+Rohan-154@users.noreply.github.com>
2022-04-02 13:42:02 +0530

    feat : added homepage

--- get-thru-todo/src/App.js
+++ get-thru-todo/src/App.js
@@ -1,10 +1,17 @@
-
-import './App.css';
-
+import "./App.css";
+import { Navbar } from "./Components/Navbar/navbar";
+import { HomePage } from "./Routes/homePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      {/* <Routes>
+        <Route path="/todos" element={<TodoList />} />
+      </Routes> */}
     </div>
   );
 }
